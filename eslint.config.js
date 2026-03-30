b963d682d36fe53837704c92b962cd6b05ff4600
Maxim <you@example.com>
2026-03-30 21:53:20 +0300

    save

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+        require: 'readonly'
+      }
+    },
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/arrow-parens': ['error', 'as-needed'],
+      '@stylistic/brace-style': ['error', '1tbs']
+    }
+  }
+];
