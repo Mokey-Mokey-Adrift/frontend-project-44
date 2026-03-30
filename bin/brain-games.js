ae8043a2a87db44c53f23467de70c2f3b06c6687
Maxim <you@example.com>
2026-03-30 22:24:55 +0300

    save

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!')
-console.log('Available games:')
-console.log('  brain-even — Answer "yes" if the number is even')
-console.log('  brain-calc — What is the result of the expression?')
-console.log('  brain-gcd — Find the greatest common divisor')
-console.log('  brain-prime — Answer "yes" if given number is prime')
-console.log('  brain-progression — What number is missing in the progression?')
+import { runGame } from '../src/index.js'
+import { generateRound } from '../src/games/even.js'
 
-process.exit(0)
+const description = 'Answer "yes" if the number is even, otherwise answer "no"'
+runGame(description, generateRound)
