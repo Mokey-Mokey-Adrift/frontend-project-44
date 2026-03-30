a3c7ea6c882a8810c8b81fa9db9b58bc4cc48f51
Maxim <you@example.com>
2026-03-30 23:14:31 +0300

    save

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,6 @@
 const getRandomNumber = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min
 
-const isPrime = num => {
+const isPrime = (num) => {
   if (num < 2) return false
   if (num === 2) return true
   if (num % 2 === 0) return false
